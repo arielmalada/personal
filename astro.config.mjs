c0cd41db7d371a2c86575208d246f12849871dc4
Ariel Malada <arielmalada@outlook.com>
2026-07-24 00:07:07 +0300

    feat: first post

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ import { remarkReadingTime } from './remark-reading-time.mjs';
 export default defineConfig({
   site: 'https://arielmalada.id',
   integrations: [mdx(), sitemap()],
+  image: {
+    domains: ['immich.arielmalada.id'],
+  },
   markdown: {
     processor: unified({ remarkPlugins: [remarkReadingTime] }),
     shikiConfig: {
